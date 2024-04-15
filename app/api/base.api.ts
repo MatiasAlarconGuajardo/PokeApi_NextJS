a4a945603ac88bfc2176d2743041279911c09679
Matias Alarcon <matias1998@hotmail.com>
2024-04-15 18:26:48 -0400

    Actualizar URL base en base.api.ts y getList en list.ts

--- app/api/base.api.ts
+++ app/api/base.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL="https://pokeapi.co/api/v2/pokemon"
+const BASE_URL=`//localhost:8000/api/pokemon/`
 
 export const instance = axios.create({
     baseURL:BASE_URL
--- app/api/list.ts
+++ app/api/list.ts
@@ -1,11 +1,10 @@
 import { instance } from "./base.api";
 
-const endpoint="offset="
 
 
 export const getPokemons={
     getList:(offset:number)=>{
-        return instance.get(`?${endpoint}${offset}&limit=9`)
+        return instance.get(`list/${offset}`)
     },
     getData:(url:string)=>{
         return instance.get(url)
